70ca611cb5b7ba6415a4ec3370d2046bc9478e72
Md Sabbir Hossain <110733872+sabbirr883@users.noreply.github.com>
2024-04-23 07:26:17 +0600

    Revert "Sajib"

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-import '../../const/app_colors.dart';
-import 'memorize_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,42 +6,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 80),
-        child: Center(
-          child: Column(
-            children: [
-              Text('Halakat',style: GoogleFonts.poppins(fontSize:28.sp,color: AppColors.greenColor,fontWeight: FontWeight.w700 ),),
-              SizedBox(
-                height: 20.h,
-              ),
-              Text('Memorize and recite \n       Quran easily',style: GoogleFonts.poppins(fontSize:18.sp,color:AppColors.offWhiteColor,fontWeight: FontWeight.w400 )),
-              SizedBox(
-                height: 50.h,
-              ),
-              Stack(
-                alignment: Alignment.bottomCenter,
-                clipBehavior: Clip.none,
-                children: [
-                  SvgPicture.asset('images/Group35.svg'),
-
-                  Positioned(
-                    bottom: -25,
-                    child: ElevatedButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>MemorizePage()));
-                    }, child: Text('Get Started',style:GoogleFonts.poppins(fontSize: 18.sp,fontWeight: FontWeight.w600),),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.buttonColor,
-                          foregroundColor: AppColors.greenColor,
-                          minimumSize: Size(185, 60),
-                        )),
-                  )
-                ],
-              ),
-
-            ],
-          ),
-        ),
+      appBar: AppBar(title: Text("Halakat"),),
+      body: ListView(
+        children: [
+          Container(
+            child: Text("Welcome"),
+          )
+        ],
       ),
     );
   }
